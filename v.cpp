a486d345a2c6c11b8a7e0dbccbda8b3e623b0492
jingduCSXQ <hui291919045@163.com>
2017-10-11 09:46:40 +0800

    Add files via upload

---
+++ v.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+const double PI=3.14;
+class cylinder{
+private:
+	double radius;
+	double height;
+public:
+	cylinder(double &radius,double &height)
+	{
+		PI*pow(radius,2)*height;
+		return 0;
+	}
+	vol()
+	{
+		cout<<"the cylinder volume is:"cout<<cylinder();
+	}
+};
+main()
+{
+	double i,j;
+	cout<<"pleater enter a number which with radius and height:"cout<<endl;
+	cin>>i>>j;
+	cylinder.cylinder(i,j);
+	cylinder.vol();
+	return 0;
+}
